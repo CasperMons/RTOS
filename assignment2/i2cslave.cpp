728bb19a63d491f0b0a4fd448e6a21273be79875
CasperMons <casper.mons@hva.nl>
2018-11-15 19:02:12 +0100

    Update i2cslave.cpp

--- assignment2/i2cslave.cpp
+++ assignment2/i2cslave.cpp
@@ -12,69 +12,65 @@
 
 std::atomic<bool> Quit = ATOMIC_VAR_INIT(false);
 
-void quit(int sig){
-    Quit = true; //will require handler quit too
+void quit(int sig) {
+	Quit = true;
 }
 
 int main(int argc, char** argv)
 {
-    CRC8 checker;
-    int receiveCounter = 0;
-    int validCounter = 0;
-    int count = 500;
+	CRC8 checker;
+	int receiveCounter = 0;
+	int validCounter = 0;
+	int count = 500;
 
-    if(argc > 1){
-	count = atoi(argv[1]);
-    }
-    std::vector<unsigned char> data;
-    signal(SIGINT, quit);
-    bsc_xfer_t xfer;
+	if (argc > 1) {
+		count = atoi(argv[1]);
+	}
+	std::vector<unsigned char> data;
+	signal(SIGINT, quit);
+	bsc_xfer_t xfer;
 
-    gpioInitialise();
+	gpioInitialise();
 
-    xfer.control = (0x0A<<16) | 0x305;
+	xfer.control = (0x0A << 16) | 0x305;
 
-    int status = bscXfer(&xfer);
-    if (status >= 0)
-    {
-        xfer.rxCnt = 0;
+	int status = bscXfer(&xfer);
+	if (status >= 0)
+	{
+		xfer.rxCnt = 0;
 
-        while(!Quit)
-        {
-            status = bscXfer(&xfer);
-            if(status)
-            {
-                if (xfer.rxCnt > 0)
-             	{
-//                  std::cout << std::endl << xfer.rxBuf << std:: endl;
-		    receiveCounter++;
+		while (!Quit)
+		{
+			status = bscXfer(&xfer);
+			if (status)
+			{
+				if (xfer.rxCnt > 0)
+				{
+					receiveCounter++;
 
-                    data.assign(xfer.rxBuf, xfer.rxBuf + BUFFLEN - 1);
+					data.assign(xfer.rxBuf, xfer.rxBuf + BUFFLEN - 1);
 
-		    checker.calculate(&data);
+					checker.calculate(&data);
 
-		    if((char)xfer.rxBuf[BUFFLEN - 1] == (char)data.back()){
-//			std::cout << "Checksum valid" << std::endl;
-			validCounter++;
-		   }else{
-//			std::cout << "Checksum invalid" << std::endl;
-		   }
-		    memset( xfer.rxBuf, '\0', sizeof(char)*BSC_FIFO_SIZE );
-                }
-            }
-         	if(receiveCounter == count){
-			break;
+					if ((char)xfer.rxBuf[BUFFLEN - 1] == (char)data.back()) {
+						validCounter++;
+					}
+					memset(xfer.rxBuf, '\0', sizeof(char)*BSC_FIFO_SIZE);
+				}
+			}
+			if (receiveCounter == count) {
+				break;
+			}
 		}
+
+		std::cout << "Received data objects: " << receiveCounter << std::endl;
+		std::cout << "Valid data objects:    " << validCounter << std::endl;
+		std::cout << "Invalid data objects:  " << receiveCounter - validCounter << std::endl;
+	}
+	else
+	{
+		std::cout << "\nInit status error.\n" << std::flush;
 	}
 
-	std::cout << "Received data objects: " << receiveCounter << std::endl;
-	std::cout << "Valid data objects:    " << validCounter << std::endl;
-	std::cout << "Invalid data objects:  " << receiveCounter - validCounter << std:: endl;
-    }
-    else
-    {
-        std::cout<<"\nInit status error.\n"<<std::flush;
-    }
-
-    return status;
+	return status;
 }
